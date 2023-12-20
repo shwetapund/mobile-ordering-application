67c323e920577b4f0a8c3ac43aff762a94e35501
shwetapund <pundshweta2020@gmail.com>
2023-12-20 04:06:42 -0800

    feat: create basic React App and setup the Routing

---
+++ client/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './views/Home/Home';
+import Login from "./views/Login/Login";
+import SignUp from "./views/SignUp/SignUp";
+import BuyNow from "./views/BuyNow/BuyNow";
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Home/>
+    },
+    {
+        path:'/login',
+        element:<Login/>
+    },
+    {
+        path:'/signup',
+        element:<SignUp/>
+    },
+    {
+        path:'/buy/:_id',
+        element:<BuyNow/>
+    }
+
+])
+
+root.render(
+    <>
+    <RouterProvider router = {router} />
+    </>
+)
+
---
+++ client/src/views/BuyNow/BuyNow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './BuyNow.css';
+
+export default function BuyNow() {
+    return(
+        <>
+        <h1>BuyNow</h1>
+        </>
+    )
+}
