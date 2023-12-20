6ef2ffe36ba5eb5194abe8c3d093df75b873328b
shwetapund <pundshweta2020@gmail.com>
2023-12-20 00:03:51 -0800

    feat: Order Api create

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,5 +1,38 @@
 import { Schema, model } from "mongoose";
 
 const orderSchema = new Schema({
-
+    user:{
+        type: Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    mobile:{
+        type:Schema.Types.ObjectId,
+        ref:'Mobile',
+        required:true
+    },
+    quantity:{
+        type:Number,
+        default:1,
+        required:true
+    },
+    deliveryCharge:{
+        type:Number,
+        default:0
+    },
+    shippingAddress : {
+        type: String,
+        required:true
+    },
+    status:{
+        type:String,
+        default:"pending"
+    }
+},
+{
+    timestamps:true
 })
+
+const Order = model('Order',orderSchema)
+
+export {Order}
